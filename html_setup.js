34a3f79e31eea070263e93d251a3aa43e9ce3f5e
Pizza Muffin <46834494+SentientMuffin@users.noreply.github.com>
2022-08-02 22:03:33 -0400

    Fix async api calls

--- html_setup.js
+++ html_setup.js
@@ -1,17 +1,18 @@
-
 function startGame() {
   gameState.start();  
 }
 
 // ================== Run =====================
 async function api() {
-  let request = new XMLHttpRequest();
-  request.open('GET', 'https://api.agify.io/?name=JSVikingChess');
-  request.send();
-  $("#API").html(request.response); 
+  let response = await fetch('https://api.agify.io/?name=JSVikingChess');
+  let data = await response.json();
+  return data;
 }
 
 $(document).ready(function() {
   startGame();
-  api();
+  api().then(data => {
+    console.log(data);
+    $("#API").html(JSON.stringify(data));
+  });
 })
