e1aba5584e8ac4b6f7e72dad7d58a415941f624b
Pizza Muffin <46834494+SentientMuffin@users.noreply.github.com>
2022-08-02 21:24:50 -0400

    Add simple api request to check
    Check whether it is possible to send api requests and receive response from github pages

--- html_setup.js
+++ html_setup.js
@@ -7,4 +7,8 @@ function startGame() {
 
 $(document).ready(function() {
   startGame();
+  let request = new XMLHttpRequest();
+  request.open('GET', 'https://api.agify.io/?name=JSVikingChess');
+  request.send();
+  $("#API").html(request.response);
 })
